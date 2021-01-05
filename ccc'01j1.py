ed3806a0e6c79aab9bbf5e51667d2eebc79584e0
stevensun1005 <1sunste@hdsb.ca>
2021-01-04 21:17:22 -0500

    Create ccc'01j1.py

---
+++ ccc'01j1.py
@@ -0,0 +1,8 @@
+def dress():
+    num = int(input())
+    for i in range(1, num + 1, 2):
+        print('*' * i + ' ' * (num * 2 - i * 2) + '*' * i)
+    for j in range (num - 2, 0, -2):
+        print('*' * j + ' ' * (num * 2 - j * 2) + '*' * j)
+
+dress()
